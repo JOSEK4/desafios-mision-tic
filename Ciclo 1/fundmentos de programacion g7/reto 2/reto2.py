9792002b2a27240d1dcb62148ba0850b41a8003a
jose ortiz <jose.a.121@hotmail.com>
2022-05-10 21:34:09 -0500

    iniciando reto 2

---
+++ Ciclo 1/fundmentos de programacion g7/reto 2/reto2.py
@@ -0,0 +1,20 @@
+# Función dentro de otra función
+
+# Ejercicio 1
+
+def imprime_Cosas():
+    # aplicacion
+    print("La clase esta genial")
+    print('Python es lo máximo')
+
+
+imprime_Cosas()
+
+
+def repetir_funciones():
+    print("\n")
+    imprime_Cosas()
+    imprime_Cosas()
+
+
+repetir_funciones()
