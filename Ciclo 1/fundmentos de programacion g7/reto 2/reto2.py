4e26d25aaf840167acd753e4f076e554311ad6af
jose ortiz <jose.a.121@hotmail.com>
2022-05-12 21:51:43 -0500

    resolucion reto 2

--- Ciclo 1/fundmentos de programacion g7/reto 2/reto2.py
+++ Ciclo 1/fundmentos de programacion g7/reto 2/reto2.py
@@ -1,20 +1,31 @@
-# Función dentro de otra función
-
-# Ejercicio 1
-
-def imprime_Cosas():
-    # aplicacion
-    print("La clase esta genial")
-    print('Python es lo máximo')
-
-
-imprime_Cosas()
-
-
-def repetir_funciones():
-    print("\n")
-    imprime_Cosas()
-    imprime_Cosas()
-
-
-repetir_funciones()
+def cliente(informacion:dict):
+    if informacion["edad"]>18:
+        boleta=20000
+        atraccion="X-Treme"
+        apto=True
+        if informacion["primer_ingreso"]== True:
+            boleta=-boleta*0.05+boleta 
+    
+    elif informacion["edad"]>=15 and informacion["edad"]<=18:
+        boleta=5000
+        atraccion="Carros chocones"
+        apto=True
+        if informacion["primer_ingreso"]== True:
+            boleta=-boleta*0.07+boleta
+        
+            
+    elif informacion["edad"]>=7 and informacion["edad"]<=15:
+        boleta=10000
+        atraccion="Sillas voladoras"
+        apto=True
+        if informacion["primer_ingreso"]== True:
+            boleta=-boleta*0.05+boleta 
+            
+    else:
+        
+        boleta="N/A "
+        atraccion="N/A" 
+        apto= False  
+    return{"nombre":informacion["nombre"],"edad":informacion["edad"],"atraccion":atraccion,"apto":apto,"primer_ingreso":informacion["primer_ingreso"],"total_boleta":boleta}  
+    
+cliente({"id_cliente":1,"nombre":"juan","edad":6,"primer_ingreso":True})   
